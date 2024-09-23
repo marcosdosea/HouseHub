d3bfa682c952db9cbb57964c882240605e73cd5c
M4RCOSVS0 <marcossvin.so@gmail.com>
2024-09-23 15:54:55 -0300

    add Service/IImovelService.cs falta o sumary ainda.

--- Código/HouseHub/Core/Service/IImovelService.cs
+++ Código/HouseHub/Core/Service/IImovelService.cs
@@ -7,10 +7,10 @@ using System.Threading.Tasks;
 namespace Core.Service {
     public interface IImovelService 
     {
-        void Create(Imovel imovel);
+        uint Create(Imovel imovel);
         void Update(Imovel imovel);
         void Delete(Imovel imovel);
-        Imovel Get(int id);
+        Imovel Get(uint id);
         IEnumerable<Imovel> GetAll();
     }
 }
--- Código/HouseHub/Service/ImovelService.cs
+++ Código/HouseHub/Service/ImovelService.cs
@@ -8,24 +8,44 @@ using System.Threading.Tasks;
 
 namespace Service {
     public class ImovelService : IImovelService {
-        public void Create(Imovel imovel) {
-            throw new NotImplementedException();
+
+        private readonly HouseHubContext houseHubContext;
+
+        public ImovelService(HouseHubContext houseHubContext)
+        {
+            this.houseHubContext = houseHubContext;
+        }
+        /// <summary>
+        /// cria um imovel
+        /// </summary>
+        /// <param name="imovel"></param>
+        /// <returns>id do imovel</returns>
+        public uint Create(Imovel imovel) {
+            houseHubContext.Imovels.Add(imovel);
+            houseHubContext.SaveChanges();
+            return imovel.Id;
         }
 
         public void Delete(Imovel imovel) {
-            throw new NotImplementedException();
+            var imovelToDelete = houseHubContext.Imovels.Find(imovel.Id);
+            if (imovelToDelete != null)
+            {
+                houseHubContext.Remove(imovelToDelete);
+                houseHubContext.SaveChanges();
+            }
         }
 
-        public Imovel Get(int id) {
-            throw new NotImplementedException();
+        public Imovel Get(uint id) {
+            return houseHubContext.Imovels.AsNoTracking().Find(id);
         }
 
         public IEnumerable<Imovel> GetAll() {
-            throw new NotImplementedException();
+            return houseHubContext.Imovels.AsNoTracking();
         }
 
         public void Update(Imovel imovel) {
-            throw new NotImplementedException();
+            houseHubContext.Imovels.Update(imovel);
+            houseHubContext.SaveChanges();
         }
     }
 }
