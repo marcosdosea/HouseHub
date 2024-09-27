968858f4e5e90649e9714c35d08b53f88cfe5114
João Victor <74694494+jvvict0r@users.noreply.github.com>
2024-09-27 15:26:57 -0300

    Solicitar avaliação de crédito
    View criado

---
+++ Código/HouseHub/HouseHubWeb/Views/Avaliacao/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HouseHubWeb.Views.Avaliacao
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
