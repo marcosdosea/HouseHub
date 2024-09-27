89b601bf761a9c27d7cffd0fb7c7e3a5344628fa
K4U4dev <okaua057.cod@gmail.com>
2024-09-27 11:06:45 -0300

    Implementação do caso de uso Buscar imovel

---
+++ Código/HouseHub/HouseHubWeb/Controllers/BuscarImovelController.cs
@@ -0,0 +1,19 @@
+﻿using Core.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseHubWeb.Controllers
+{
+    public class BuscarImovelController : Controller
+    {
+        private readonly IImovelService imovelService;
+
+        public BuscarImovelController(IImovelService imovelService)
+        {
+            this.imovelService = imovelService;
+        }
+        public IActionResult BuscarImovel(IImovelService imovelService)
+        {
+            return View();
+        }
+    }
+}
---
+++ Código/HouseHub/HouseHubWeb/Models/BuscarImovelViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace HouseHubWeb.Models
+{
+    public class BuscarImovelViewModel
+    {
+        public string Cidade { get; set; } = string.Empty;
+        public string Bairro { get; set; } = string.Empty;
+        public decimal ValorMaximo { get; set; }
+        public byte Quartos { get; set; }
+    }
+}
