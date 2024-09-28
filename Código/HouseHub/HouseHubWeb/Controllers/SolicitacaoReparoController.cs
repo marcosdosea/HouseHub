66147fad9532c618b4ebf391213a39e6057c082b
imarcossm <viniciusmorais.com@gmail.com>
2024-09-28 19:15:34 -0300

    Implementado o caso de uso SolicitacaoReparo.
    closed #135

---
+++ Código/HouseHub/HouseHubWeb/Controllers/SolicitacaoReparoController.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Core;
+using Core.Service;
+using HouseHubWeb.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseHubWeb.Controllers
+{
+    public class SolicitacaoReparoController : Controller
+    {
+        private readonly ISolicitacaoreparoService solicitacaoreparoService;
+        private readonly IMapper mapper;
+
+        public SolicitacaoReparoController(ISolicitacaoreparoService solicitacaoreparoService, IMapper mapper)
+        {
+            this.solicitacaoreparoService = solicitacaoreparoService;
+            this.mapper = mapper;
+        }
+
+        // GET: SolicitacaoReparoController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: SolicitacaoReparoController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SolicitacaoReparoController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SolicitacaoReparoController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(SolicitacaoReparoViewModel solicitacaoReparo)
+        {
+            try
+            {
+                var solicitacao = mapper.Map<Solicitacaoreparo>(solicitacaoReparo);
+                solicitacaoreparoService.Create(solicitacao);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
