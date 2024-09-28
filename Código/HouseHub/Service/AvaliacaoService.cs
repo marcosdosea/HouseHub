f9670cca388b77e3e32bfa3911501f55d000bc9d
MarcosVinicius-Mota <marcosvinicius.mota652790@gmail.com>
2024-09-27 21:54:42 -0300

    Implementado a avaliação service closed #113

--- Código/HouseHub/Service/AvaliacaoService.cs
+++ Código/HouseHub/Service/AvaliacaoService.cs
@@ -29,48 +29,5 @@ namespace Service
             houseHubContext.SaveChanges();
             return avaliacao.Id;
         }
-
-        /// <summary>
-        /// Exclui uma Avaliação existente.
-        /// </summary>
-        /// <param name="avaliacao">O objeto Avaliação a ser excluído.</param>
-        public void Delete(Avaliacao avaliacao)
-        {
-            var _avaliacao = houseHubContext.Avaliacaos.Find(avaliacao.Id);
-            if (_avaliacao != null)
-            {
-                houseHubContext.Remove(_avaliacao);
-                houseHubContext.SaveChanges();
-            }
-        }
-
-        /// <summary>
-        /// Recupera uma Avaliação pelo seu ID.
-        /// </summary>
-        /// <param name="id">O ID da Avaliação a ser recuperada.</param>
-        /// <returns>A Avaliação recuperada, ou nulo se não encontrada.</returns>
-        public Avaliacao? Get(int id)
-        {
-            return houseHubContext.Avaliacaos.Find(id);
-        }
-
-        /// <summary>
-        /// Recupera todas as Avaliações.
-        /// </summary>
-        /// <returns>Um IEnumerable de objetos Avaliação.</returns>
-        public IEnumerable<Avaliacao> GetAll()
-        {
-            return houseHubContext.Avaliacaos.AsNoTracking();
-        }
-
-        /// <summary>
-        /// Atualiza uma Avaliação já existente.
-        /// </summary>
-        /// <param name="avaliacao">Objeto Avaliação que será atualizado.</param>
-        public void Update(Avaliacao avaliacao)
-        {
-            houseHubContext.Update(avaliacao);
-            houseHubContext.SaveChanges();
-        }
     }
 }
