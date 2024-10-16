5f060bbb189c0f10721fb6b3d63ddba46526f632
M4RCOSVS0 <marcossvin.so@gmail.com>
2024-10-15 21:57:00 -0300

    Finalizado o caso de uso de LocarImovel
    essa PR foi feita em Programação em Par
    closed #137
    
    Co-Authored-By: Marcos Vinícius Mota <112988242+MarcosVinicius-Mota@users.noreply.github.com>

---
+++ Código/HouseHub/Core/DTOs/ImovelDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs
+{
+    public class ImovelDto
+    {
+        public uint IdImovel { get; set; }
+        public decimal Iptu { get; set; }
+        public decimal PrecoAluguel { get; set; }
+        public decimal PrecoCondominio { get; set; }
+        public string NomeUsuario { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+    }
+}
---
+++ Código/HouseHub/HouseHubWeb/Mappers/LocacaoProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Core.DTOs;
+using HouseHubWeb.Models;
+
+namespace HouseHubWeb.Mappers
+{
+    public class LocacaoProfile : Profile
+    {
+        public LocacaoProfile()
+        {
+            CreateMap<ImovelDto, LocacaoViewModel>().ReverseMap();
+        }
+    }
+}
