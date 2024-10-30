906a897da03a083208e3377afe3975a972eeb4a4
MarcosVinicius-Mota <marcosvinicius.mota652790@gmail.com>
2024-10-29 21:31:01 -0300

    Tela refatorada closed #186

---
+++ Código/HouseHub/Core/DTOs/PessoaDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs
+{
+    public class PessoaDto
+    {
+        public uint Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Cpf { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public decimal RendaAprovada { get; set; }
+    }
+}
--- Código/HouseHub/Core/Service/IPessoaService.cs
+++ Código/HouseHub/Core/Service/IPessoaService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,5 +14,7 @@ namespace Core.Service {
         Pessoa? Get(uint id);
         IEnumerable<Pessoa> GetAll();
         IEnumerable<Pessoa> GetByNome(string nome);
+        PessoaDto ? GetByCpf(string cpf);
+        PessoaDto ? GetByEmail(string email);
     }
 }
